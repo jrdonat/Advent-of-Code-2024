7db5d86120948943e2475db64eb835653d1a9cc7
jrdonat <jrddonat@gmail.com>
2024-12-04 01:04:28 -0500

    Day 2 Solutions

---
+++ Solutions/Day 2/Part 1.py
@@ -0,0 +1,27 @@
+INPUT = "Inputs\\Day 2.txt"
+
+def is_safe_line(levels):
+    initial_direction = 1 if levels[0] > levels[1] else -1
+    
+    for i in range(len(levels) - 1):
+        if abs(levels[i] - levels[i+1]) > 3 or levels[i] == levels[i+1]:
+            return False
+        
+        current_direction = 1 if levels[i] > levels[i+1] else -1
+        if current_direction != initial_direction:
+            return False
+    
+    return True
+
+def count_safe_lines(filename):
+    with open(filename, 'r') as file:
+        lines = file.readlines()
+        safe_lines = sum(1 for line in lines if is_safe_line(list(map(int, line.strip().split()))))
+    
+    return safe_lines
+
+def main():
+    print(f"Safe levels: {count_safe_lines(INPUT)}")
+
+if __name__ == "__main__":
+    main()
