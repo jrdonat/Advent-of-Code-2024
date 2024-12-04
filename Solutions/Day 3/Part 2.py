96be6982f1d5c18aef8b31e7c97a75ae1f354bd3
jrdonat <jrddonat@gmail.com>
2024-12-04 02:01:13 -0500

    Fixed Day 3

--- Solutions/Day 3/Part 2.py
+++ Solutions/Day 3/Part 2.py
@@ -8,28 +8,27 @@ def main():
     disableRe = r"don't\(\)"
 
     with open(INPUT, 'r') as file:
-        lines = file.readlines()
+        line = file.read().replace('\n', '')
         total = 0
-        for line in lines:
-            while True:
-                enable_match = re.search(enableRe, line)
-                disable_match = re.search(disableRe, line)
-                if not disable_match:
-                    break
-                if not enable_match:
-                    line = line[:disable_match.start()]
-                    break
-                if enable_match.start() > disable_match.start():
-                    line = line[:disable_match.start()] + line[enable_match.end():]
-                else:
-                    break
+        while True:
+            enable_match = re.search(enableRe, line)
+            disable_match = re.search(disableRe, line)
+            if not disable_match:
+                break
+            if not enable_match:
+                line = line[:disable_match.start()]
+                break
+            if enable_match.start() > disable_match.start():
+                line = line[:disable_match.start()] + line[enable_match.end():]
+            else:
+                line = line[:enable_match.start()] + line[enable_match.end():]
 
-            matches = re.findall(regex, line)
-            for match in matches:
-                nums = match.split("(")[1].split(")")[0].split(",")
-                result = int(nums[0]) * int(nums[1])
-                total += result
-        print(total)
+        matches = re.findall(regex, line)
+        for match in matches:
+            nums = match.split("(")[1].split(")")[0].split(",")
+            result = int(nums[0]) * int(nums[1])
+            total += result
+    print(total)
 
 if __name__ == "__main__":
     main()
